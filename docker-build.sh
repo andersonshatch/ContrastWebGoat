6bb5766011c85caa12fcf52a18d18aba20c2d806
Josh Anderson <josh.anderson@contrastsecurity.com>
2021-02-16 16:32:40 +0000

    Bump version of Java agent to latest

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --build-arg CONTRAST_AGENT_VERSION=3.7.10.17525 --iidfile .image_id .
+docker build --build-arg CONTRAST_AGENT_VERSION=3.8.1.18696 --iidfile .image_id .
